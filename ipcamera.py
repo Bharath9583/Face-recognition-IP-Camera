95647a0f1e4f76435d682dbcecb2e8fe6b0edbc5
Bharath9583 <bharathkumar.kamal@gmail.com>
2017-04-16 10:35:57 +0530

    Added xml and python file

---
+++ ipcamera.py
@@ -0,0 +1,31 @@
+import cv2
+import urllib
+import numpy as np
+import time
+
+stream = urllib.urlopen('http://192.168.2.188:8080/video?x.mjpeg')
+detector= cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+bytes = bytes()
+while True:
+    bytes += stream.read(1024)
+    a = bytes.find(b'\xff\xd8')
+    b = bytes.find(b'\xff\xd9')
+    if a != -1 and b != -1:
+        jpg = bytes[a:b+2]
+        bytes = bytes[b+2:]
+        i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+        gray = cv2.cvtColor(i, cv2.COLOR_BGR2GRAY)
+        faces = detector.detectMultiScale(gray, 1.3, 5)
+        count = 0
+        if(len(faces)!=0):
+            for (x,y,w,h) in faces:
+                cv2.rectangle(i,(x,y),(x+w,y+h),(255,0,0),2)
+                count = count+1
+            cv2.putText(i,str(count),(x,y+h), font, 1,(255,255,255),2)
+        cv2.imshow('frame',i)
+        cv2.waitKey(1)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            exit(0)
+        
